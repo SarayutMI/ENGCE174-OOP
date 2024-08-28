f5b0e2506db29728fd216f3500e2622b0868c886
Sarayut Meepanya <108066406+SarayutMI@users.noreply.github.com>
2024-08-28 19:10:25 +0700

    Add files via upload

---
+++ OOP_Lab4/Unit_5.2_List_Comprehensions.py
@@ -0,0 +1,12 @@
+#Example 1 : Creating and manipulating lists
+
+#Creating a list of number
+numbers = [1,2,3,4,5]
+
+#Adding elements to the end of the list
+numbers.append(6)
+
+#Modifying an element at a specific index
+numbers.remove(4)
+
+print("Updated list: ",numbers) # Output : [1,2,10,5,6]
